04957c5d1f4c5aeefbfcc1618bb6b343a48da75e
@devnbugs <devnbugs@gmail.com>
2026-03-19 06:41:37 +0000

    stopx

---
+++ kycappx/app/Contracts/VerificationProviderInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Contracts;
+
+class VerificationProviderInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ kycappx/app/Services/Verification/VerificationOrchestrator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Verification;
+
+class VerificationOrchestrator
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
